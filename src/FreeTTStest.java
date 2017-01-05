4dd06a5676f31f73b974c45121ae223ff9ca7f17
Harold Martin <harold.martin92@gmail.com>
2017-01-05 16:56:58 -0500

    clened gitignore

--- src/FreeTTStest.java
+++ src/FreeTTStest.java
@@ -7,17 +7,20 @@ public class FreeTTStest {
 	 private static final String VOICENAME_kevin = "kevin16";
 	 	 
 	 public static void main(String[] args) {
+		 
+		 System.setProperty("mbrola.base", "/media/DATA/SoftwareLibraries/freetts-1.2.2-bin/MBROLA"); 
 	 
-	  String text = "FreeTTS was written by the Sun Microsystems Laboratories "
-	    + "Speech Team and is based on CMU's Flite engine. Hello!!";
-	  
-	  Voice voice;
-	  VoiceManager voiceManager = VoiceManager.getInstance();
-	  voice = voiceManager.getVoice(VOICENAME_kevin);
-	  voice.allocate();
-	  
-//	  System.out.println(voice.speak(text));
-	  voice.speak("which way should I go?");
+		  String text = "FreeTTS was written by the Sun Microsystems Laboratories "
+		    + "Speech Team and is based on CMU's Flite engine. Hello!!";
+		  
+		  Voice voice;
+		  VoiceManager voiceManager = VoiceManager.getInstance();
+	//	  voice = voiceManager.getVoice(VOICENAME_kevin);
+		  voice = voiceManager.getVoice("mbrola_us2");
+		  voice.allocate();
+		  
+		  System.out.println(voice.speak(text));
+		  voice.speak("which way should I go?");
 	  	  
 	 }
 	 
