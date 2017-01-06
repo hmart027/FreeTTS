b841168dba4e8e58ef7e3eb4ce5f8a9b58703f1d
Harold Martin <harold.martin92@gmail.com>
2017-01-05 22:55:58 -0500

    Set TTS volume

--- src/FreeTTStest.java
+++ src/FreeTTStest.java
@@ -13,18 +13,16 @@ public class FreeTTStest {
 		 
 		 //Set the location of the Mbrola voices
 		 System.setProperty("mbrola.base", "/media/DATA/SoftwareLibraries/freetts-1.2.2-bin/MBROLA"); 
-	 
-		  String text = "FreeTTS was written by the Sun Microsystems Laboratories "
-		    + "Speech Team and is based on CMU's Flite engine. Hello!!";
-		  
-		  final Voice voice;
+	 		  
 		  VoiceManager voiceManager = VoiceManager.getInstance();
-	//	  voice = voiceManager.getVoice(VOICENAME_kevin);
-		  voice = voiceManager.getVoice("mbrola_us2");
+		  final Voice voice = voiceManager.getVoice("mbrola_us2");
 		  voice.allocate();
+		  
+		  voice.setVolume((float) 0.8);
 		  
 //		  System.out.println(voice.speak(text));
 //		  voice.speak("which way should I go?");
+		  
 		  
 		  TextInputGUI gui = new TextInputGUI();
 		  gui.setTalkListener(new TextInputGUI.TalkListener() {
